70717f991337aee69cb8ab96ddc81149dac8b9db
Goldstin <90073810+Goldstin@users.noreply.github.com>
2021-10-13 09:54:02 -0500

    Update signIn.js

--- pages/signIn.js
+++ pages/signIn.js
@@ -6,15 +6,15 @@ import { loginUser } from '../firebase/auth'
 import { Button } from '@chakra-ui/button'
 import { Input } from '@chakra-ui/input'
 import { Flex, Heading } from '@chakra-ui/layout'
-import { useColorMode, useColorModeValue } from '@chakra-ui/color-mode'
+import { Box } from "@chakra-ui/react"
 import { useToast } from "@chakra-ui/react"
 import { useRouter } from "next/router"
+import { BsFillEnvelopeFill } from 'react-icons/fa'
 
 export default function loginScreen({ navigation }) {
     const [email, setEmail] = useState({ value: '', error: '' })
     const [password, setPassword] = useState({ value: '', error: '' })
     const [loading, setLoading] = useState()
-    const formBackground = useColorModeValue("gray.100", "gray.700")
     const toast = useToast()
     const router = useRouter()
     var successSignIn = false
@@ -69,16 +69,26 @@ export default function loginScreen({ navigation }) {
     }
 
     return (
-        <Flex height="100vh" alignItems="center" justifyContent="center">
-            <Flex direction="column" background={formBackground} p={12} rounded={6}>
-                <Button size="xs" textAlign={'left'} justifyContent="left" variant="link" onClick={() => {router.push("./")}} mb={6} colorScheme='teal'>&lt;-Back</Button>
-                <Heading textAlign={'center'} mb={6}>Sign In</Heading>
-                <Input placeholder="E-mail" variant="filled" onChange={(event) => setEmail({ ...email, value: event.currentTarget.value })} mb={3} type="email" />
-                <Input placeholder="Password" variant="filled" onChange={(event) => setPassword({ ...password, value: event.currentTarget.value })} mb={6} type="password" />
-                <Button isLoading={loading} onClick={onLoginPressed} mb={6} colorScheme='teal'>Login</Button>
-                <Button variant="link" onClick={() => {router.push("./resetpasswordpage")}} mb={2} colorScheme='teal'>Forgot password?</Button>
-                <Button variant="link" onClick={() => {router.push("./signUp")}} colorScheme='teal'>Don't have an account? Register now!</Button>
+        
+        <><Box textColor="black" fontSize="32" textAlign="center" w="100%" p={10} color="black">
+
+        </Box>
+        <Box textColor="black" fontSize="32" textAlign="center" w="100%" p={1} color="black">
+                Welcome to Healthtok
+        </Box>
+        <Box textColor="black" fontSize="24" textAlign="center" w="100%" p={0} color="black">
+                Made to inspire, share your story today
+        </Box>
+        <Flex height="50vh" alignItems="center" justifyContent="center">
+            <Flex direction="column" p={12} rounded={6}>
+                <Heading mt={450} fontSize="14" textColor="black" textAlign={'left'} mb={1}>EMAIL ADDRESS:</Heading>
+                <Input focusBorderColor="gray.900" variant="filled" onChange={(event) => setEmail({ ...email, value: event.currentTarget.value })} mb={10} type="email" />
+                <Heading fontSize="14" textColor="black" textAlign={'left'} mb={1}>PASSWORD:</Heading>
+                <Input variant="filled" onChange={(event) => setPassword({ ...password, value: event.currentTarget.value })} mb={6} type="password" />
+                <Button isLoading={loading} onClick={onLoginPressed} mb={2} background="gray.900" textColor='white'>Login</Button>
+                <Button variant="link" textAlign="right" onClick={() => { router.push("./resetpasswordpage") } } mb={300} textColor='gray'>Forgot password?</Button>
+                <Button variant="link" onClick={() => { router.push("./signUp") } } colorScheme='blackAlpha'>Not a member? Sign up now</Button>
             </Flex>
-        </Flex>
+        </Flex></>
     )
 }
